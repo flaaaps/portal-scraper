925b0f6c2ff85e1f7af9c225f0310bcd53558375
flaaaps <julianwening4@gmail.com>
2020-11-18 17:57:29 +0100

    🐛 Set args for deployment

--- routes/index.js
+++ routes/index.js
@@ -9,29 +9,34 @@ router.get('/', (req, res) => {
 
 router.get('/:day/:hour?', async (req, res) => {
   const { day, hour } = req.params;
-
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
-  await page.goto(`http://127.0.0.1:5500/index.html`);
-  const selector = '#asam_content > div > table > tbody > tr';
+  try {
+    await page.goto(`http://127.0.0.1:5500/index.html`);
+    const selector = '#asam_content > div > table > tbody > tr';
 
-  const result = await page.$$eval(selector, (trs) =>
-    trs.map((tr) => {
-      const tds = [...tr.getElementsByTagName('td')];
-      const row = [];
-      tds.map((td) => {
-        console.log(td);
-        if (td.textContent.trim().length > 0) row.push(td.innerText.trim().replaceAll('\n', ' '));
-        else row.push(null);
-      });
-      return row;
-    })
-  );
-  const data = format(result, day, hour);
+    const result = await page.$$eval(selector, (trs) =>
+      trs.map((tr) => {
+        const tds = [...tr.getElementsByTagName('td')];
+        const row = [];
+        tds.map((td) => {
+          console.log(td);
+          if (td.textContent.trim().length > 0) row.push(td.innerText.trim().replaceAll('\n', ' '));
+          else row.push(null);
+        });
+        return row;
+      })
+    );
+    const data = format(result, day, hour);
 
-  await browser.close();
+    await browser.close();
 
-  res.send({ sucess: true, data });
+    res.send({ sucess: true, data });
+  } catch (error) {
+    res.send({ success: false, error: error.message });
+  }
 });
 
 /* 
